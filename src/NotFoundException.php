aca55ec19c991fbaf066c3d243ba511939116a73
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2016-07-03 20:08:30 +0200

    Remove error trigger

--- src/NotFoundException.php
+++ src/NotFoundException.php
@@ -2,8 +2,6 @@
 
 namespace Http\Discovery;
 
-@trigger_error('The '.__NAMESPACE__.'\NotFoundException class is deprecated since version 1.0 and will be removed in 2.0. Use Http\Discovery\Exception\NotFoundException instead.', E_USER_DEPRECATED);
-
 /**
  * Thrown when a discovery does not find any matches.
  *
