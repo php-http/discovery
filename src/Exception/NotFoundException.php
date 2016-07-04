b79c5e45130a6ca5c39efc019080d99ff40f06eb
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2016-07-04 09:12:06 +0200

    Add final notice

--- src/Exception/NotFoundException.php
+++ src/Exception/NotFoundException.php
@@ -5,7 +5,7 @@ namespace Http\Discovery\Exception;
 /**
  * Thrown when a discovery does not find any matches.
  *
- * @final
+ * @final do NOT extend this class, not final for BC reasons
  *
  * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
  */
