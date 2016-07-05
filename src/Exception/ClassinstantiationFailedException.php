a8d976f3f76eadf4ea6e9a70495d8a6e91d6dc44
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-07-05 09:32:40 +0200

    Added "implements exception"

--- src/Exception/ClassinstantiationFailedException.php
+++ src/Exception/ClassinstantiationFailedException.php
@@ -2,11 +2,13 @@
 
 namespace Http\Discovery\Exception;
 
+use Http\Discovery\Exception;
+
 /**
  * Thrown when a class fails to instantiate.
  *
  * @author Tobias Nyholm <tobias.nyholm@gmail.com>
  */
-class ClassinstantiationFailedException extends \RuntimeException
+class ClassinstantiationFailedException extends \RuntimeException implements Exception
 {
 }
