2ba86a53fba16592af189386ccee23942979fc2e
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-07-05 09:05:26 +0200

    Added tests

--- spec/ClassDiscoverySpec.php
+++ spec/ClassDiscoverySpec.php
@@ -12,6 +12,8 @@ use Puli\Discovery\Api\Discovery;
 use Puli\Repository\Api\ResourceRepository;
 use PhpSpec\ObjectBehavior;
 use spec\Http\Discovery\Helper\DiscoveryHelper;
+use spec\Http\Discovery\Helper\FailedDiscoveryStrategy;
+use spec\Http\Discovery\Helper\SuccessfullDiscoveryStrategy;
 
 class ClassDiscoverySpec extends ObjectBehavior
 {
@@ -33,7 +35,7 @@ class ClassDiscoverySpec extends ObjectBehavior
         $this->shouldThrow(DiscoveryFailedException::class)->duringFind('NoCandidate');
     }
 
-    function it_returns_a_class(DiscoveryStrategy $strategy)
+    function it_returns_a_class()
     {
         $candidate = ['class' => 'ClassName', 'condition' => true];
         DiscoveryHelper::setClasses('Foobar', [$candidate]);
@@ -41,7 +43,7 @@ class ClassDiscoverySpec extends ObjectBehavior
         $this->find('Foobar')->shouldReturn('ClassName');
     }
 
-    function it_validates_conditions(DiscoveryStrategy $strategy) {
+    function it_validates_conditions() {
         $c0 = ['class' => 'ClassName0', 'condition' => false];
         $c1 = ['class' => 'ClassName1', 'condition' => true];
         $c2 = ['class' => 'ClassName2', 'condition' => false];
@@ -49,7 +51,36 @@ class ClassDiscoverySpec extends ObjectBehavior
 
         $this->find('Foobar')->shouldReturn('ClassName1');
     }
+
+    function it_prepends_strategies() {
+        $candidate = ['class' => 'Added'];
+        DiscoveryHelper::setClasses('Foobar', [$candidate]);
+
+        ClassDiscovery::setStrategies([SuccessfullDiscoveryStrategy::class]);
+        ClassDiscovery::prependStrategy(DiscoveryHelper::class);
+
+        $this->find('Foobar')->shouldReturn('Added');
+    }
+
+    function it_appends_strategies() {
+        $candidate = ['class' => 'Added'];
+        DiscoveryHelper::setClasses('Foobar', [$candidate]);
+
+        // Make sure our strategy is added to the list
+        ClassDiscovery::setStrategies([FailedDiscoveryStrategy::class]);
+        ClassDiscovery::appendStrategy(DiscoveryHelper::class);
+
+        $this->find('Foobar')->shouldReturn('Added');
+
+        // Make sure it is added last in the list
+        ClassDiscovery::setStrategies([SuccessfullDiscoveryStrategy::class]);
+        ClassDiscovery::appendStrategy(DiscoveryHelper::class);
+
+        $this->find('Foobar')->shouldReturn('Success');
+    }
 }
+
+
 
 class ClassDiscoveryStub extends ClassDiscovery
 {
