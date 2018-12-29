5d4df6f046ba5073b141ccc4f354e80f2d0e90c9
Nyholm <tobias.nyholm@gmail.com>
2018-12-29 11:13:23 +0100

    Make sure one can find a PSR-18 client

--- src/HttpClientDiscovery.php
+++ src/HttpClientDiscovery.php
@@ -4,6 +4,7 @@ namespace Http\Discovery;
 
 use Http\Client\HttpClient;
 use Http\Discovery\Exception\DiscoveryFailedException;
+use Psr\Http\Client\ClientInterface;
 
 /**
  * Finds an HTTP Client.
@@ -26,6 +27,28 @@ final class HttpClientDiscovery extends ClassDiscovery
         } catch (DiscoveryFailedException $e) {
             throw new NotFoundException(
                 'No HTTPlug clients found. Make sure to install a package providing "php-http/client-implementation". Example: "php-http/guzzle6-adapter".',
+                0,
+                $e
+            );
+        }
+
+        return static::instantiateClass($client);
+    }
+
+    /**
+     * Finds a PSR-18 HTTP Client.
+     *
+     * @return ClientInterface
+     *
+     * @throws Exception\NotFoundException
+     */
+    public static function findPsr18Client()
+    {
+        try {
+            $client = static::findOneByType(ClientInterface::class);
+        } catch (DiscoveryFailedException $e) {
+            throw new \Http\Discovery\Exception\NotFoundException(
+                'No PSR-18 clients found. Make sure to install a package providing "psr/http-client-implementation". Example: "php-http/guzzle6-adapter".',
                 0,
                 $e
             );
