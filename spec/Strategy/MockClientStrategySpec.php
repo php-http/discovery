68045ba8456bdb899d67986ba77be0603064129e
Pascal Woerde <pwoerde@tilaa.com>
2019-09-23 11:26:12 +0200

    Support PSR-18 interface as candidate by inheritance

--- spec/Strategy/MockClientStrategySpec.php
+++ spec/Strategy/MockClientStrategySpec.php
@@ -8,6 +8,7 @@ use Http\Discovery\ClassDiscovery;
 use Http\Discovery\Strategy\DiscoveryStrategy;
 use Http\Discovery\Strategy;
 use PhpSpec\ObjectBehavior;
+use Psr\Http\Client\ClientInterface;
 use spec\Http\Discovery\Helper\DiscoveryHelper;
 
 class MockClientStrategySpec extends ObjectBehavior
@@ -20,6 +21,13 @@ class MockClientStrategySpec extends ObjectBehavior
     function it_should_return_the_mock_client(DiscoveryStrategy $strategy)
     {
         $candidates = $this->getCandidates(HttpClient::class);
+        $candidates->shouldBeArray();
+        $candidates->shouldHaveCount(1);
+    }
+
+    function it_should_return_the_mock_client_for_psr_interface(DiscoveryStrategy $strategy)
+    {
+        $candidates = $this->getCandidates(ClientInterface::class);
         $candidates->shouldBeArray();
         $candidates->shouldHaveCount(1);
     }
